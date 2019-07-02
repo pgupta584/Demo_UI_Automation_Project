ef6fa2d9513a5d5c46d602ad3755f90fbce56153
PG <pg@BBMDVPANKAJGUP.local>
2019-07-02 19:03:21 +0530

    updated  with Vietnam config

--- Demo_Project/src/main/java/ALL_Page/Login_Page.java
+++ Demo_Project/src/main/java/ALL_Page/Login_Page.java
@@ -4,6 +4,8 @@ import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.testng.Reporter;
+
 import Test_Base.TestBase;
 
 public class Login_Page extends TestBase
@@ -14,11 +16,25 @@ public class Login_Page extends TestBase
 		this.driver=driver;
 	}
 	
-	public void LoginToHome(String user,String Pass)
+	public void LoginToHome(String user,String Pass,String OTP)
 	{
 		getWebelement("Username_Text_id").sendKeys(user);
+		Reporter.log("Enter User Name Steps completed");
+		getWebelement("Next_Button_xpath").click();
+		Reporter.log("Clicked on Next Button Steps completed");
+		getWebelement("OTP_Text_id").sendKeys(OTP);
+		Reporter.log("Enter OTP Steps completed");
+		getWebelement("Login_Button_Verify_xpath").click();
+		Reporter.log("Verify OTP Steps completed");
 		getWebelement("Password_Text_id").sendKeys(Pass);
+		Reporter.log("Enter Password Steps completed");
 		getWebelement("Login_Button_xpath").click();
 		driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
+	}
+	public static String getLoginPage()
+	{
+		String value=getWebelement("Home_PageText_xpath").getText();
+		Reporter.log("Capture the Home Page Steps completed");
+		return value;
 	}
 }
--- Demo_Project/src/test/java/Login_Tests/Login_Test.java
+++ Demo_Project/src/test/java/Login_Tests/Login_Test.java
@@ -2,6 +2,8 @@ package Login_Tests;
 
 import java.io.IOException;
 
+import org.testng.Assert;
+import org.testng.Reporter;
 import org.testng.annotations.Test;
 import ALL_Page.Login_Page;
 import Data_Reader.ReadExcel_Data;
@@ -14,12 +16,15 @@ public class Login_Test extends TestBase
 	public void LoginToHomePage() throws IOException
 	{
 		lp=new Login_Page(driver);
-		//lp.loginPage("admin", "manager");
+		lp.LoginToHome("938837779", "Testing@123", "0000");
 		//Get Data from Excel
-		String user=ReadExcel_Data.getMapData("User");
+		//String user=ReadExcel_Data.getMapData("User");
 		String pass=ReadExcel_Data.getMapData("Password");
 		System.out.println("---------------------"+pass);
-		lp.LoginToHome(user,pass);
+		//lp.LoginToHome(user,pass);
+		String expectedValue="Help & Support";
+		Assert.assertEquals(Login_Page.getLoginPage(), expectedValue);
+		Reporter.log("Login is Success");
 	}
 	@Test
 	public void InvalidLoginToHomePage()
